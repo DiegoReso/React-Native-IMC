43863b83d6fba53e0c22903bb6770adaa77e5907
Diego Reis <diegoreso@me.com>
2023-07-06 13:43:17 -0300

    create the structure

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View,Text} from 'react-native';
+import Title from './src/components/Title';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Title/>
+      
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffb',
     alignItems: 'center',
     justifyContent: 'center',
   },
